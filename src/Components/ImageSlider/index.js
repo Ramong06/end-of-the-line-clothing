58e492dc0442114fd1c260cd2f61fe773e53e363
RG-Mac <ramong5406@gmail.com>
2021-01-24 15:28:17 -0600

    added folders for image slider and slider data

---
+++ src/Components/ImageSlider/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+
+const ImageSlider = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ImageSlider;
---
+++ src/Components/SliderData/index.js
@@ -0,0 +1,24 @@
+export const SliderData = [
+    {
+        image: './assets/EOL.jpeg'
+    },
+    {
+        image: './assets/IMG_5072.jpg'
+    },
+    {
+        image: './assets/IMG_4795.jpg'
+    },
+    {
+        image: './assets/IMG_4816.jpg'
+    },
+    {
+        image: './assets/IMG_4826.jpg'
+    },
+    {
+        image: './assets/IMG_4946.jpg'
+    },
+    {
+        image: './assets/IMG_4835.jpg'
+    },
+];
+
