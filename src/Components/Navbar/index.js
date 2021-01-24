3eecff9ddac5de127c531277336feb236ddbb42b
RG-Mac <ramong5406@gmail.com>
2021-01-23 22:40:44 -0600

    changed logo height

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import './style.css';
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
-import Logo from '../../assets/EOL.PNG'; // with import
+import Logo from '../../assets/EOL.jpeg';
 
 
 const Navigation = () => {
     return (
         <div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Navbar.Brand href="#home"><img src={ Logo } alt='' height='100px' /></Navbar.Brand>
+            <Navbar className='navBar' collapseOnSelect expand="lg" bg="light" variant="light">
+                <Navbar.Brand href="#home"><img src={ Logo } alt='' height='105px' /></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
-                    <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                    <Nav className="mr-auto" id='navbar'>
+                    {/* <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link> */}
+                    </Nav>
+                    <Nav className='nav__menu'>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
                     </NavDropdown>
-                    </Nav>
-                    <Nav>
                     <Nav.Link href="#deets">More deets</Nav.Link>
                     <Nav.Link eventKey={2} href="#memes">
                         Dank memes
