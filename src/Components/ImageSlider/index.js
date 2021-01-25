a17289db28a9ef1d84cbd2c2fb3d963644a9c9b9
RG-Mac <ramong5406@gmail.com>
2021-01-25 11:49:16 -0600

    added functionality to arrows

--- src/Components/ImageSlider/index.js
+++ src/Components/ImageSlider/index.js
@@ -9,17 +9,31 @@ const ImageSlider = ({ slides }) => {
     const [current, setCurrent] = useState(0);
     const length = slides.length
 
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    };
+
+    console.log(current);
+
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    };
+
     return (
         <hero className='hero__slider'>
-            <ArrowBackIosIcon className="backArrow" />
-            <ArrowForwardIosIcon className="forwardArrow" />
-        { SliderData.map((slide, index) => {
-            return (
-              <img src={ slide.image } alt='eol-item' className='hero__sliderImage' />
-          )  
-        })}
+            <ArrowBackIosIcon className="backArrow" onClick={prevSlide}/>
+            <ArrowForwardIosIcon className="forwardArrow" onClick={nextSlide} />
+            { SliderData.map((slide, index) => {
+                return (
+                    <img src={slide.image} alt='eol-item' className='hero__sliderImage' />
+                )
+            })}
         </hero>
     )
-}
+};
 
 export default ImageSlider;
