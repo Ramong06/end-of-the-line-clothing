e1d2967d32696a83b768d091028b7edbaa5bbe24
RG-Mac <ramong5406@gmail.com>
2021-01-25 11:01:05 -0600

    styling photos for slider

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import './App.css';
 import ImageSlider from "./Components/ImageSlider";
 import Navbar from './Components/Navbar';
+import { SliderData } from "./Components/SliderData";
 
 const App =() => {
   return (
 
       <div>
       <Navbar />
-      <ImageSlider />
+      <ImageSlider slides={SliderData} />
       </div>
 
   );
--- src/Components/ImageSlider/index.js
+++ src/Components/ImageSlider/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import { SliderData } from '../SliderData';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 
 
-const ImageSlider = () => {
+const ImageSlider = ({ slides }) => {
+    const [current, setCurrent] = useState(0);
+    const length = slides.length
+
     return (
-        <>
+        <hero className='hero__slider'>
+            <ArrowBackIosIcon className="backArrow" />
+            <ArrowForwardIosIcon className="forwardArrow" />
         { SliderData.map((slide, index) => {
             return (
-              <img src={ slide.image } alt='eol-item' />
+              <img src={ slide.image } alt='eol-item' className='hero__sliderImage' />
           )  
         })}
-        </>
+        </hero>
     )
 }
 
